ee295ed34beabdd49b430e64cc3f1bd2de7a7896
leozeballez <zeballezleonel@hotmail.com>
2021-06-20 07:29:59 -0300

    Requerimientos 1 y 2 realizados y testeados. Falta agregar los analizadores (requerimiento 3)

--- src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
@@ -14,12 +14,27 @@ class Pedido(val ip: String, val url: String, val fechaHora: LocalDateTime)
 class Respuesta(val codigo: CodigoHttp, val body: String, val tiempo: Int, val pedido: Pedido)
 
 class ServidorWeb {
+  val protocoloAceptado: String = "http:"
+  val modulos = mutableListOf<Modulo>()
+
+  //Requerimientos 1 y 2 implementados.
+
   fun realizarPedido(pedido: Pedido): Respuesta {
-    if(!pedido.url.startsWith("http")) {
-      val respuesta = Respuesta(CodigoHttp.NOT_IMPLEMENTED, "", 10, pedido)
-      return respuesta
+    if(!pedido.url.startsWith(protocoloAceptado)) {
+      return Respuesta(CodigoHttp.NOT_IMPLEMENTED, "", 10, pedido)
     }
-    val respuesta = Respuesta(CodigoHttp.OK, "", 10, pedido)
-    return respuesta
+    val moduloSeleccionado = this.buscarModuloQueSoporteExtension(pedido.url)
+    if(moduloSeleccionado == null) {
+      return Respuesta(CodigoHttp.NOT_FOUND, "", 10, pedido)
+    }
+    return Respuesta(CodigoHttp.OK, moduloSeleccionado.body, moduloSeleccionado.tiempoQueTarda, pedido)
   }
+
+  fun buscarModuloQueSoporteExtension(url: String): Modulo? {
+    return modulos.find { m -> m.puedeTrabajarCon(url) }
+  }
+}
+
+class Modulo(val extensiones: Collection<String>, val body: String, val tiempoQueTarda: Int) {
+  fun puedeTrabajarCon(url: String) = extensiones.any { ext -> url.endsWith(ext) }
 }
--- src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
@@ -1,7 +1,7 @@
 package ar.edu.unahur.obj2.servidorWeb
 
 import io.kotest.core.spec.style.DescribeSpec
-import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
 import java.time.LocalDateTime
 
 class ServidorWebTest : DescribeSpec({
@@ -9,18 +9,34 @@ class ServidorWebTest : DescribeSpec({
     val servidorWeb1 = ServidorWeb()
     val pedido1 = Pedido("207.46.13.5", "http://pepito.com.ar/documentos/doc1.html", LocalDateTime.now())
     val pedido2 = Pedido("107.40.17.8", "https://pepito.com.ar/documentos/doc3.html", LocalDateTime.now())
+    val pedido3 = Pedido("147.50.27.1", "http://pepito.com.ar/documentos/foto1.jpg", LocalDateTime.now())
+    val modulo1 = Modulo(setOf<String>("html"), "modulo web", 5)
+    servidorWeb1.modulos.add(modulo1)
 
     // Requerimiento 1
     describe("realiza un pedido") {
       it("y devuelve 200 ya que el protocolo es el correcto (HTTP).") {
-        servidorWeb1.realizarPedido(pedido1).should { Respuesta(CodigoHttp.OK, "", 10, pedido1) }
-        servidorWeb1.realizarPedido(pedido1) === Respuesta(CodigoHttp.OK, "", 10, pedido1)
+        val respuesta = servidorWeb1.realizarPedido(pedido1)
+        respuesta.codigo.shouldBe(CodigoHttp.OK)
+        respuesta.body.shouldBe("modulo web")
+        respuesta.pedido.shouldBe(pedido1)
+        respuesta.tiempo.shouldBe(5)
       }
       it("y devuelve 501 ya que el protocolo NO es el correcto (HTTPS).") {
-        servidorWeb1.realizarPedido(pedido2).should { Respuesta(CodigoHttp.NOT_IMPLEMENTED, "", 10, pedido2) }
-        servidorWeb1.realizarPedido(pedido2) === Respuesta(CodigoHttp.NOT_IMPLEMENTED, "", 10, pedido2)
+        val respuesta = servidorWeb1.realizarPedido(pedido2)
+        respuesta.codigo.shouldBe(CodigoHttp.NOT_IMPLEMENTED)
+        respuesta.body.shouldBe("")
+        respuesta.pedido.shouldBe(pedido2)
+        respuesta.tiempo.shouldBe(10)
       }
-      // el shouldBe() no funciona para testear.
+      // Requerimiento 2 ya implementado y se agrega el 404.
+      it("y devuelve 404 ya que no hay ningun modulo que pueda atender el pedido.") {
+        val respuesta = servidorWeb1.realizarPedido(pedido3)
+        respuesta.codigo.shouldBe(CodigoHttp.NOT_FOUND)
+        respuesta.body.shouldBe("")
+        respuesta.pedido.shouldBe(pedido3)
+        respuesta.tiempo.shouldBe(10)
+      }
     }
   }
 })
